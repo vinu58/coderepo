cc34b5b24e311f3ac6794bb4e6d5836be3cf4729
vinu <mohan.vinu58@gmail.com>
2019-11-04 22:35:34 +0530

    updation

--- script.sh
+++ script.sh
@@ -5,5 +5,5 @@ cd /Users/vinumohandas/Documents/Kubernetes/react-app/testdemo
 git clone 'https://github.com/vinu58/coderepo.git'
 
 cd coderepo
-docker build -t 'coderepo' .
-docker run -p 8000:8000 -i coderepo
+docker build -v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker -t 'coderepo' .
+docker run -d -v /var/run/docker.sock:/var/run/docker.sock  -v /usr/bin/docker:/usr/bin/docker -p 8000:8000 -i coderepo
