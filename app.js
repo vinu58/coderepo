6958b80f655fa5f053774cd0b1ba2dac3ecc565a
vinu <mohan.vinu58@gmail.com>
2019-11-05 15:16:43 +0530

    test changes

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const path = __dirname + '/views/';
 const port = 8000;
 
-router.use(function (req,res,next) {
-  console.log('/' + req.method);
-  next();
-});
+#router.use(function (req,res,next) {
+#  console.log('/' + req.method);
+#  next();
+#});
 
 router.get('/', function(req,res){
   res.sendFile(path + 'index.html');
